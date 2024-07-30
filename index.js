fcd9ae94d1d6e3f5db31f6c2fe7b9e509f9009d4
Muhammad Taha <76904648+MuhammadTaha27@users.noreply.github.com>
2024-07-30 19:12:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-const WebSocket = require("ws");
-const http = require("http");
-const os = require("os");
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const WebSocket = require('ws');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+app.use(express.static(path.join(__dirname, '/public')));
 
-function getServerIp() {
-  const interfaces = os.networkInterfaces();
-  for (let iface in interfaces) {
-    for (let alias of interfaces[iface]) {
-      if (alias.family === 'IPv4' && !alias.internal) {
-        return alias.address;
-      }
-    }
-  }
-  return '127.0.0.1'; // Fallback to localhost if no external IP is found
-}
-// HTTP server
-const server = http.createServer(app);
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
 
-// WebSocket server
-const wsServer = new WebSocket.Server({ server });
-
-// array of connected websocket clients
-let connectedClients = [];
-let audioStream = [];
-
-wsServer.on("connection", (ws, req) => {
+wss.on('connection', function (ws) {
   console.log("Connected");
   connectedClients.push(ws);
   ws.on("message", (data) => {
@@ -42,19 +25,13 @@ wsServer.on("connection", (ws, req) => {
       }
     });
   });
-  ws.on("close", () => {
-    audioStream.push(null); // Signal the end of the stream
+
+  ws.on('close', function () {
+    console.log('stopping client interval');
+    clearInterval(id);
   });
 });
 
-// HTTP stuff
-app.use("/image", express.static("image"));
-app.use("/js", express.static("js"));
-app.get("/audio", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "./audio_client.html"))
-);
-
-server.listen(PORT, () => {
-  const ip = getServerIp();
-  console.log(`Server listening at http://${ip}:${PORT}`);
+server.listen(8080, function () {
+  console.log('Listening on http://0.0.0.0:8080');
 });
