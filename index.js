0e2400287a8e67176adfe52a8f88ddee7197cb9e
Muhammad Taha <76904648+MuhammadTaha27@users.noreply.github.com>
2024-07-30 19:09:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
-'use strict';
-
-const express = require('express');
-const path = require('path');
-const { createServer } = require('http');
-
-const WebSocket = require('ws');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const WebSocket = require("ws");
+const http = require("http");
+const os = require("os");
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
+const PORT = process.env.PORT || 8000;
 
-const server = createServer(app);
-const wss = new WebSocket.Server({ server });
+function getServerIp() {
+  const interfaces = os.networkInterfaces();
+  for (let iface in interfaces) {
+    for (let alias of interfaces[iface]) {
+      if (alias.family === 'IPv4' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return '127.0.0.1'; // Fallback to localhost if no external IP is found
+}
+// HTTP server
+const server = http.createServer(app);
 
-wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignoring errors.
-      //
+// WebSocket server
+const wsServer = new WebSocket.Server({ server });
+
+// array of connected websocket clients
+let connectedClients = [];
+let audioStream = [];
+
+wsServer.on("connection", (ws, req) => {
+  console.log("Connected");
+  connectedClients.push(ws);
+  ws.on("message", (data) => {
+    audioStream.push(data);
+    connectedClients.forEach((client, i) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      } else {
+        connectedClients.splice(i, 1);
+      }
     });
-  }, 100);
-  console.log('started client interval');
-
-  ws.on('close', function () {
-    console.log('stopping client interval');
-    clearInterval(id);
+  });
+  ws.on("close", () => {
+    audioStream.push(null); // Signal the end of the stream
   });
 });
 
-server.listen(8080, function () {
-  console.log('Listening on http://0.0.0.0:8080');
+// HTTP stuff
+app.use("/image", express.static("image"));
+app.use("/js", express.static("js"));
+app.get("/audio", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./audio_client.html"))
+);
+
+server.listen(PORT, () => {
+  const ip = getServerIp();
+  console.log(`Server listening at http://${ip}:${PORT}`);
 });
